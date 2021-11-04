88adb31e0249893012682bbc49dfbc4165ca52d9
joseazrom <azromjose@gmail.com>
2021-11-04 15:41:24 -0300

    proyecto node

---
+++ src/helpers/auth.js
@@ -0,0 +1,9 @@
+export const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash("error_msg", "Not Authorized.");
+  res.redirect("/users/signin");
+};
+
+module.esports = helpers;
